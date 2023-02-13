bb3b8e8a656cfa62d8588a752e76eee0750784e0
sovetbekoff <b.sovetbekoff@gmail.com>
2023-02-13 16:14:48 +0300

    sidebar

---
+++ src/components/Content/SideBar/SideBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Box, Container } from "@mui/material";
+import "./SideBar.css";
+const SideBar = () => {
+  return (
+    <div>
+      <Box className="container">
+        <div className="filter">Фильтровать по тэгам</div>
+        <input className="inp" placeholder="поиск тэгов" />
+        <span className="preventSearch">prevent search</span>
+      </Box>
+    </div>
+  );
+};
+
+export default SideBar;
