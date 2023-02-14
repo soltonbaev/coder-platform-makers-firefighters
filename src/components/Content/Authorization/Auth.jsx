d20775885ab707cc4040b36b631bd115157d93cb
nurdas00 <nurdas00@gmail.com>
2023-02-14 17:16:15 +0600

    .

--- src/components/Content/Authorization/Auth.jsx
+++ src/components/Content/Authorization/Auth.jsx
@@ -1,7 +1,108 @@
-import React from 'react';
-
+import React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { Button, Link } from "@mui/material";
 const Auth = () => {
-   return <div></div>;
+  return (
+    <div>
+      <Box
+        sx={{
+          margin: "20px",
+        }}
+      >
+        <h1>Регистрация</h1>
+        <center>
+          <input
+            className="inp-place"
+            type="text"
+            placeholder="Имя"
+            style={{
+              backgroundColor: "#AA6800",
+              borderStyle: "none",
+              borderRadius: "7px",
+            }}
+          />
+        </center>
+        <center>
+          <input
+            className="inp-place"
+            type="text"
+            placeholder="Фамилия"
+            style={{
+              backgroundColor: "#AA6800",
+              borderStyle: "none",
+              borderRadius: "7px",
+            }}
+          />
+        </center>
+        <center>
+          <input
+            className="inp-place"
+            type="text"
+            placeholder="Юзернэйм*"
+            style={{
+              backgroundColor: "#AA6800",
+              borderStyle: "none",
+              borderRadius: "7px",
+            }}
+          />
+        </center>
+        <center>
+          <input
+            className="inp-place"
+            type="text"
+            placeholder="Почтовый Адрес*"
+            style={{
+              backgroundColor: "#AA6800",
+              borderStyle: "none",
+              borderRadius: "7px",
+            }}
+          />
+        </center>
+        <center>
+          <input
+            className="inp-place"
+            type="text"
+            placeholder="Пароль*"
+            style={{
+              backgroundColor: "#AA6800",
+              borderStyle: "none",
+              borderRadius: "7px",
+            }}
+          />
+        </center>
+        <center>
+          <input
+            className="inp-place"
+            type="text"
+            placeholder="Поддтвердить пароль"
+            style={{
+              backgroundColor: "#AA6800",
+              borderStyle: "none",
+              borderRadius: "7px",
+            }}
+          />
+        </center>
+        <center>
+          <Button
+            style={{
+              backgroundColor: "#474747",
+              borderStyle: "none",
+              borderRadius: "7px",
+              width: "250px",
+              height: "35px",
+              color: "#fffdfd",
+            }}
+          >
+            Cоздать
+          </Button>
+        </center>
+        <Link href="#" variant="body2">
+          Have an account? Sign In.
+        </Link>
+      </Box>
+    </div>
+  );
 };
 
 export default Auth;
--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
-
+import { Box } from "@mui/material";
+import React from "react";
+import Logo from "../Header/logo.svg";
 const logo = () => {
-   return <div></div>;
+  return (
+    <>
+      <Box
+        sx={{
+          width: { xs: "60%", md: "30%" },
+          display: "flex",
+          justifyContent: { xs: "flex", md: "flex-start" },
+        }}
+      >
+        <Box
+          sx={{
+            width: { xs: "100%", sm: "50%" },
+            paddingRight: { xs: "5px" },
+          }}
+        >
+          <img src={Logo} width="100%" />
+        </Box>
+      </Box>
+    </>
+  );
 };
 
 export default logo;
