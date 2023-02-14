1f65ce9422ff2326fa0e1ea01e73087362b4f2da
nurdas00 <nurdas00@gmail.com>
2023-02-14 17:53:11 +0600

    .

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
   return (
     <div className="App">
       <NavBar />
+      <SideBar />
+      <Auth />
       <Footer />
     </div>
   );
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,8 @@ import { Box } from "@mui/system";
 import React from "react";
 import BgImage from "../Footer/fireextwhite copy flipped rotated orange copy 1.png";
 import Logo from "../Footer/makers logo white 1.png";
+import firePng from "../Footer/FIREFIGHTERS.png";
+import { Button, Link } from "@mui/material";
 
 const Footer = () => {
   return (
@@ -12,9 +14,9 @@ const Footer = () => {
           display: "flex",
           height: "350px",
           width: "100vw",
+          flexDirection: "column",
           backgroundColor: "rgb(71 71 71)",
           backgroundImage: `${BgImage}`,
-          //  justifyContent: "end",
         }}
       >
         <img
@@ -25,8 +27,73 @@ const Footer = () => {
             left: "0",
           }}
         />
-        <Box>
-          <img src={Logo} />
+        <Box
+          className="futter"
+          sx={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "space-around",
+            marginTop: "20px",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <img src={Logo} />
+            <img src={firePng} />
+          </Box>
+          <Box>
+            <h4>Навигация</h4>
+            <p>Навигация</p>
+            <p>Навигация</p>
+            <p>Навигация</p>
+            <p>Навигация</p>
+          </Box>
+          <Box>
+            <h4>Навигация</h4>
+            <p>Навигация</p>
+            <p>Навигация</p>
+          </Box>
+          <Box>
+            <h4>Навигация</h4>
+            <p>Навигация</p>
+            <p>Навигация</p>
+            <p>Навигация</p>
+          </Box>
+          <Box>
+            <h4>Навигация</h4>
+            <p>Навигация</p>
+            <p>Навигация</p>
+            <p>Навигация</p>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            marginRight: "30%",
+          }}
+        >
+          <Button
+            sx={{
+              color: " #ffffff",
+              border: "1px solid white",
+              borderRadius: "8px",
+            }}
+          >
+            Задать вопрос{" "}
+          </Button>
+        </Box>
+        <Box
+          sx={{
+            marginLeft: "10%",
+          }}
+        >
+          <p>
+            @ 2023 - проект создан командами из PY24: Николай, Артур, Александр
+            JS28: Бекмырза, Эрнас, Ибраим
+          </p>
         </Box>
       </Box>
     </div>
