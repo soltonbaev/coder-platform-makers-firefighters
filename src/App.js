b186d2686bbc18909c5da1a4a5b9b9864eea9fea
sovetbekoff <b.sovetbekoff@gmail.com>
2023-02-14 18:26:30 +0300

    questionPage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
 import Auth from "./components/Content/Authorization/Auth";
+import QuestionPage from "./components/Content/QA/QuestionPage";
 import SideBar from "./components/Content/QA/Sidebar.jsx";
-import Footer from "./components/Footer/Footer";
+// import Footer from "./components/Footer/Footer";
 import NavBar from "./components/Header/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <Footer />
+    <div className="App" style={{ display: "flex" }}>
+      <SideBar />
+      <QuestionPage />
+      {/* <NavBar /> */}
+      {/* <Footer /> */}
     </div>
   );
 }
