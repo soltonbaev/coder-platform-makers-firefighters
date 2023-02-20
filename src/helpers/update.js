7fe017ce13b155918a4e94f56e89a348f96c004b
soltonbaev <isoltonbaev@gmail.com>
2023-02-20 13:42:05 +0600

    .

--- src/helpers/update.js
+++ src/helpers/update.js
@@ -1,13 +1,13 @@
-import axios from "axios";
-import { PROFILE } from "./globals";
-import { getAccessConfig } from "./read";
+import axios from 'axios';
+import {PROFILE} from './globals';
+import {getAccessConfig} from './read';
 export async function updateUser(id, formData) {
-  try {
-    let config = getAccessConfig();
-    console.log(config);
-    const res = await axios.patch(PROFILE + id, formData, config);
-    console.log(res);
-  } catch (error) {
-    console.log(error);
-  }
+   try {
+      let config = getAccessConfig();
+      console.log(config);
+      const res = await axios.patch(PROFILE + id, formData, config);
+      console.log(res);
+   } catch (error) {
+      console.log(error);
+   }
 }
