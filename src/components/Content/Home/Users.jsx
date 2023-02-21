c4199294a528853b20f3e8b9ed21856cf50ccbc1
soltonbaev <isoltonbaev@gmail.com>
2023-02-21 11:02:31 +0600

    .

--- src/components/Content/Home/Users.jsx
+++ src/components/Content/Home/Users.jsx
@@ -1,176 +1,58 @@
-
-import { Avatar, Grid, Pagination } from "@mui/material";
-import { Container } from "@mui/system";
-import axios from "axios";
+import {Avatar, Grid, Pagination} from '@mui/material';
+import {Container} from '@mui/system';
+import axios from 'axios';
 import React, {
-  useEffect,
-  useReducer,
-  useState,
-  createContext,
-  useContext,
-} from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { USER_LIST } from "../../../helpers/globals";
-import Fire from "./images/fireexting 1.svg";
-import bita from "./images/image 5.svg";
+   useEffect,
+   useReducer,
+   useState,
+   createContext,
+   useContext,
+} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import {USER_LIST} from '../../../helpers/globals';
+import Fire from './images/fireexting 1.svg';
+import bita from './images/image 5.svg';
 
 export const userContext = createContext();
 export const useUserContext = () => useContext(userContext);
 
 const INIT_STATE = {
-  users: [],
+   users: [],
 };
 
 function reducer(state = INIT_STATE, action) {
-  switch (action.type) {
-    case "GET_USERS":
-      return {
-        ...state,
-        users: action.payload.results,
-        //   pages: Math.ceil(action.payload.count / 6),
-      };
-  }
+   switch (action.type) {
+      case 'GET_USERS':
+         return {
+            ...state,
+            users: action.payload.results,
+            //   pages: Math.ceil(action.payload.count / 6),
+         };
+   }
 }
 
 const Users = () => {
+   const [state, dispatch] = useReducer(reducer, INIT_STATE);
+   const [user, setUser] = useState('');
+   const usersList = state.users;
 
-  const [state, dispatch] = useReducer(reducer, INIT_STATE);
-  const [user, setUser] = useState("");
-  const usersList = state.users;
+   const navigate = useNavigate();
+   //   console.log(usersList);
+   useEffect(() => {
+      getUsers();
+   }, []);
 
-  const navigate = useNavigate();
-  //   console.log(usersList);
-  useEffect(() => {
-    getUsers();
-  }, []);
+   const getUsers = async () => {
+      let {data} = await axios(USER_LIST);
+      dispatch({
+         type: 'GET_USERS',
+         payload: data,
+      });
+   };
 
-  const getUsers = async () => {
-    let { data } = await axios(USER_LIST);
-    dispatch({
-      type: "GET_USERS",
-      payload: data,
-    });
-  };
-
-  return (
-    <Container maxWidth="lg" sx={{ display: "flex", textAlign: "baseline" }}>
-      <div>
-        <Grid
-          sx={{
-            padding: "3%",
-            display: "block",
-            backgroundColor: " rgba(217, 217, 217, 1)",
-            // width: "48%",
-            // height: "100%",
-            textAlign: "center",
-            borderRadius: "8px",
-            width: "70%",
-            height: "45%",
-            marginTop: "8%",
-          }}
-        >
-          <Grid item>Фильтрация пользователей</Grid>
-          <Grid
-            style={{
-              display: "flex",
-              flexWrap: "wrap",
-
-              //   width: "60%",
-            }}
-          >
-            <span
-              style={{
-                border: "1px solid rgba(0, 70, 5, 1)",
-                color: "rgba(0, 70, 5, 1)",
-                borderRadius: "8px",
-                marginLeft: "5%",
-                marginTop: "5%",
-                //  fontFamily: "Raleway",
-                //  fontSize: " 12px",
-                //  fontWeight: " 400",
-                //  lineHeight: " 14px",
-                //  textAlign: "center",
-              }}
-            >
-              по репутации
-            </span>{" "}
-            <span
-              style={{
-                border: "1px solid rgba(0, 70, 5, 1)",
-                color: "rgba(0, 70, 5, 1)",
-                borderRadius: "8px",
-                marginLeft: "5%",
-                marginTop: "5%",
-              }}
-              // onClick={mentor}
-            >
-              новые
-            </span>{" "}
-            <span
-              style={{
-                border: "1px solid rgba(0, 70, 5, 1)",
-                color: "rgba(0, 70, 5, 1)",
-                borderRadius: "8px",
-                marginLeft: "5%",
-                marginTop: "5%",
-              }}
-            >
-              голосующие
-            </span>{" "}
-            <span
-              style={{
-                border: "1px solid rgba(0, 70, 5, 1)",
-                color: "rgba(0, 70, 5, 1)",
-                borderRadius: "8px",
-                marginLeft: "5%",
-                marginTop: "5%",
-              }}
-            >
-              админы
-            </span>{" "}
-            <span
-              style={{
-                border: "1px solid rgba(0, 70, 5, 1)",
-                color: "rgba(0, 70, 5, 1)",
-                borderRadius: "8px",
-                marginLeft: "5%",
-                marginTop: "5%",
-              }}
-            >
-              трекеры
-            </span>{" "}
-            <span
-              style={{
-                border: "1px solid rgba(0, 70, 5, 1)",
-                color: "rgba(0, 70, 5, 1)",
-                borderRadius: "8px",
-                marginLeft: "5%",
-                marginTop: "5%",
-              }}
-            >
-              менторы
-            </span>
-            <span
-              style={{
-                border: "1px solid rgba(0, 70, 5, 1)",
-                color: "rgba(0, 70, 5, 1)",
-
-                borderRadius: "8px",
-                marginLeft: "5%",
-                marginTop: "5%",
-              }}
-              s
-            >
-              кураторы
-            </span>
-          </Grid>
-        </Grid>
-      </div>
-      <div style={{ display: "block", width: "100%", marginRight: "4px" }}>
-        <h1>Пользователи</h1>
-        <Grid container style={{ display: "flex", flexWrap: "wrap" }}>
-          {usersList.map((user) => (
-
+   return (
+      <Container maxWidth="lg" sx={{display: 'flex', textAlign: 'baseline'}}>
+         <div>
             <Grid
                sx={{
                   padding: '3%',
@@ -218,6 +100,7 @@ const Users = () => {
                         marginLeft: '5%',
                         marginTop: '5%',
                      }}
+                     // onClick={mentor}
                   >
                      новые
                   </span>{' '}
@@ -259,6 +142,7 @@ const Users = () => {
                         border: '1px solid rgba(0, 70, 5, 1)',
                         color: 'rgba(0, 70, 5, 1)',
                         borderRadius: '8px',
+
                         marginLeft: '5%',
                         marginTop: '5%',
                      }}
@@ -428,15 +312,11 @@ const Users = () => {
                      </div>
                   </Grid>
                ))}
+               {/* <Pagination count={10} /> */}
             </Grid>
-
-          ))}
-          {/* <Pagination count={10} /> */}
-        </Grid>
-      </div>
-    </Container>
-  );
-
+         </div>
+      </Container>
+   );
 };
 
 export default Users;
