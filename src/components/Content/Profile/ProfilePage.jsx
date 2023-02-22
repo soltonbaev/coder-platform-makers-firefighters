61f04e13af4d3f71e30d3194e313e7e8ca26fba1
soltonbaev <isoltonbaev@gmail.com>
2023-02-23 00:10:26 +0600

    .

--- src/components/Content/Profile/ProfilePage.jsx
+++ src/components/Content/Profile/ProfilePage.jsx
@@ -190,79 +190,59 @@ const ProfilePage = () => {
                            // marginTop: '1.5rem',
                         }
                      }
-
                   >
-                    Показать все группы...
-                  </Typography>
-                
-                </Link>
-              </Grid>
-            </Grid>
-            <Grid
-              className="right-column"
-              item
-              sm={9}
-              md={9}
-              lg={9}
-              sx={
-                {
-                  // marginTop: '1.5rem',
-                }
-              }
-            >
-              <Grid
-                item
-                sx={{
-                  display: "flex",
-                  gap: "1rem",
-                  alignItems: "center",
-                }}
-              >
-                <Grid item>
-                  <Avatar
-                    sx={{ width: "10rem", height: "10rem" }}
-                    alt={user.username}
-                    src={user.user_photo}
-                  />
-                </Grid>
-                <Grid item sx={{}}>
-                  <Grid item>
-                    <Grid
-                      item
-                      sx={{
-                        display: "flex",
-                        gap: "1rem",
-                        alignItems: "center",
-                        flexWrap: "wrap",
-                      }}
-                    >
-                      <h1 style={{ padding: "0", margin: "0" }}>
-                        {user.name} {user.last_name}
-                      </h1>
-
-                      <Button
-                        onClick={() => navigate("/editProfile")}
+                     <Grid
+                        item
                         sx={{
-                          height: "2rem",
-                          color: "#474747",
-                          border: "1px solid #474747",
-                          borderRadius: "5px",
+                           display: 'flex',
+                           gap: '1rem',
+                           alignItems: 'center',
                         }}
-                      >
-                        Редактировать профиль
-                      </Button>
-                    </Grid>
-                    <Typography>@{user.username}</Typography>
-                  </Grid>
+                     >
+                        <Grid item>
+                           <Avatar
+                              sx={{width: '10rem', height: '10rem'}}
+                              alt={user.username}
+                              src={user.user_photo}
+                           />
+                        </Grid>
+                        <Grid item sx={{}}>
+                           <Grid item>
+                              <Grid
+                                 item
+                                 sx={{
+                                    display: 'flex',
+                                    gap: '1rem',
+                                    alignItems: 'center',
+                                    flexWrap: 'wrap',
+                                 }}
+                              >
+                                 <h1 style={{padding: '0', margin: '0'}}>
+                                    {user.name} {user.last_name}
+                                 </h1>
 
-                  <Typography
-                    sx={{
-                      color: "#AA6800",
-                    }}
-                  >
-                    Участник c {user.date_joined} года
-                  </Typography>
+                                 <Button
+                                    onClick={() => navigate('/editProfile')}
+                                    sx={{
+                                       height: '2rem',
+                                       color: '#474747',
+                                       border: '1px solid #474747',
+                                       borderRadius: '5px',
+                                    }}
+                                 >
+                                    Редактировать профиль
+                                 </Button>
+                              </Grid>
+                              <Typography>@{user.username}</Typography>
+                           </Grid>
 
+                           <Typography
+                              sx={{
+                                 color: '#AA6800',
+                              }}
+                           >
+                              Участник c {user.date_joined} года
+                           </Typography>
 
                            <Box
                               sx={{
@@ -314,7 +294,6 @@ const ProfilePage = () => {
          )}
       </>
    );
-
 };
 
 export default ProfilePage;
