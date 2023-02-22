e88842f71d1b6f92adc4fd571005982d607b0942
sovetbekoff <b.sovetbekoff@gmail.com>
2023-02-22 18:35:37 +0300

    .

--- src/components/Content/Home/Questions.jsx
+++ src/components/Content/Home/Questions.jsx
@@ -7,71 +7,79 @@ import {
   Typography,
 } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import "./q.css";
 import RenderQuestion from "./RenderQuestion";
 import SideBar from "./Sidebar";
-import { getQuestions } from "../../../helpers/read";
 
-// const questions = [
-//    {
-//       question: 'Как вывести нечетные числа в javascript',
-//       votesCount: 5,
-//       answersCount: 3,
-//       viewsCount: 10,
-//       tags: ['javascript', 'even numbers', 'loop'],
-//       username: 'codewarrior',
-//       id: 1,
-//    },
-//    {
-//       question: 'Как в Питоне превратить строку в массив',
-//       votesCount: 10,
-//       answersCount: 2,
-//       viewsCount: 50,
-//       tags: ['python', 'string', 'array'],
-//       username: 'noobiest_noob',
-//       id: 2,
-//    },
-//    {
-//       question: 'Как добавить в html-элемент несколько классов?',
-//       votesCount: 90,
-//       answersCount: 7,
-//       viewsCount: 60,
-//       tags: ['html', 'classes'],
-//       username: 'meowmeow',
-//       id: 3,
-//    },
-//    {
-//       question: 'Как сгенерировать случайное число в Python?',
-//       votesCount: 9,
-//       answersCount: 5,
-//       viewsCount: 70,
-//       tags: ['python', 'random number'],
-//       username: 'axios',
-//       id: 4,
-//    },
-//    {
-//       question: 'Как найти одинаковые элементы в массиве js?',
-//       votesCount: 40,
-//       answersCount: 13,
-//       viewsCount: 133,
-//       tags: ['javascript', 'array'],
-//       username: 'jsexpert',
-//       id: 5,
-//    },
-// ];
+import { useGlobalContext } from "../../../contexts/GlobalContextProvider";
+import { getQuestions, getQuestionsRaw } from "../../../helpers/read";
+
 const Questions = () => {
-  console.clear();
   // console.group("Questions group");
+  const [page, setPage] = useState(1);
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
+
+  const { setShowToast, setErrorType, setToastMessage } = useGlobalContext();
+
+  function setToast(showToast, errorType, toastMessage) {
+    setShowToast(showToast);
+    setErrorType(errorType);
+    setToastMessage(toastMessage);
+  }
 
   const navigate = useNavigate();
+
   const [questions, setQuestions] = useState("");
+
   useEffect(() => {
     getQuestions().then((res) => {
+      if (res.name === "AxiosError") {
+        setToast(true, "error", res.message);
+        return;
+      }
       setQuestions(res);
       console.log("getQuestions res", res);
     });
   }, []);
+
+  const [count, setCount] = useState();
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchParams, setSearchParams] = useSearchParams();
+  let total = Math.ceil(count / 10);
+
+  useEffect(() => {
+    getQuestionsRaw().then((res) => {
+      setQuestions(res.results);
+      console.log("getQuestions res", res);
+    });
+  }, []);
+
+  useEffect(() => {
+    getQuestionsRaw().then((res) => {
+      setQuestions(res.results);
+      console.log("getQuestions res", res);
+    });
+  }, [searchParams]);
+
+  useEffect(async () => {
+    let data = await getQuestionsRaw();
+    setCount(data.count);
+  }, []);
+
+  // useEffect(() => {
+  //   getQuestions();
+  // }, [searchParams]);
+
+  useEffect(() => {
+    setSearchParams({
+      page: currentPage,
+    });
+    console.log(currentPage);
+  }, [currentPage]);
 
   return (
     <Container maxWidth="lg" sx={{ minHeight: "60vh" }}>
@@ -125,7 +133,11 @@ const Questions = () => {
               })}
           </Box>
           <Box>
-            <Pagination />
+            <Pagination
+              count={total}
+              page={currentPage}
+              onChange={(e, p) => setCurrentPage(p)}
+            />
           </Box>
         </Grid>
       </Grid>
--- src/components/Content/Home/Users.jsx
+++ src/components/Content/Home/Users.jsx
@@ -1,20 +1,13 @@
 import { Avatar, Box, Grid, Pagination } from "@mui/material";
 import { Container } from "@mui/system";
 import axios from "axios";
-import React, {
-  useEffect,
-  useReducer,
-  useState,
-  createContext,
-  useContext,
-} from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { async } from "q";
+import React, { useEffect, useReducer, useState } from "react";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
 import { USER_LIST } from "../../../helpers/globals";
+import { getQuestionsRaw } from "../../../helpers/read";
 import Fire from "./images/fireexting 1.svg";
 import bita from "./images/image 5.svg";
-
-export const userContext = createContext();
-export const useUserContext = () => useContext(userContext);
 
 const INIT_STATE = {
   users: [],
@@ -25,288 +18,295 @@ function reducer(state = INIT_STATE, action) {
     case "GET_USERS":
       return {
         ...state,
-        users: action.payload.results,
-        //   pages: Math.ceil(action.payload.count / 6),
+        users: action.payload,
       };
   }
 }
 
 const Users = () => {
   const [state, dispatch] = useReducer(reducer, INIT_STATE);
-  const [user, setUser] = useState("");
-  const usersList = state.users;
 
   const navigate = useNavigate();
-  //   console.log(usersList);
+
   useEffect(() => {
     getUsers();
   }, []);
 
   const getUsers = async () => {
     let { data } = await axios(USER_LIST);
+    console.log(data);
     dispatch({
       type: "GET_USERS",
-      payload: data,
+      payload: data.results,
+    });
+  };
+
+  const isMentors = () => {
+    let arr = state.users.filter((user) => user.is_mentor);
+    dispatch({
+      type: "GET_USERS",
+      payload: arr,
+    });
+    // console.log("arr", arr);
+  };
+
+  const newUsers = () => {
+    let array = state.users.sort((a, b) => b.id - a.id);
+    dispatch({
+      type: "GET_USERS",
+      payload: array,
     });
   };
 
   return (
-    <Container maxWidth="lg" sx={{ display: "flex", textAlign: "baseline" }}>
-      <div>
-        <Grid
-          sx={{
-            padding: "3%",
-            display: "block",
-            backgroundColor: " rgba(217, 217, 217, 1)",
-            // width: "48%",
-            // height: "100%",
-            textAlign: "center",
-            borderRadius: "8px",
-            width: "70%",
-            height: "45%",
-            marginTop: "8%",
-          }}
-        >
-          <Grid item>Фильтрация пользователей</Grid>
+    <Container maxWidth="lg">
+      <div
+        style={{
+          display: "flex",
+          textAlign: "baseline",
+          justifyContent: "space-around",
+        }}
+      >
+        <div style={{ width: "24%" }}>
           <Grid
-            style={{
-              display: "flex",
-              flexWrap: "wrap",
-
-              //   width: "60%",
+            sx={{
+              padding: "3%",
+              display: "block",
+              backgroundColor: " rgba(217, 217, 217, 1)",
+              // width: "48%",
+              // height: "100%",
+              textAlign: "center",
+              borderRadius: "8px",
+              width: "100%",
+              height: "45%",
+              marginTop: "8%",
             }}
           >
-            <span
-              style={{
-                border: "1px solid rgba(0, 70, 5, 1)",
-                color: "rgba(0, 70, 5, 1)",
-                borderRadius: "8px",
-                marginLeft: "5%",
-                marginTop: "5%",
-                //  fontFamily: "Raleway",
-                //  fontSize: " 12px",
-                //  fontWeight: " 400",
-                //  lineHeight: " 14px",
-                //  textAlign: "center",
-              }}
-            >
-              по репутации
-            </span>{" "}
-            <span
-              style={{
-                border: "1px solid rgba(0, 70, 5, 1)",
-                color: "rgba(0, 70, 5, 1)",
-                borderRadius: "8px",
-                marginLeft: "5%",
-                marginTop: "5%",
-              }}
-              // onClick={mentor}
-            >
-              новые
-            </span>{" "}
-            <span
-              style={{
-                border: "1px solid rgba(0, 70, 5, 1)",
-                color: "rgba(0, 70, 5, 1)",
-                borderRadius: "8px",
-                marginLeft: "5%",
-                marginTop: "5%",
-              }}
-            >
-              голосующие
-            </span>{" "}
-            <span
-              style={{
-                border: "1px solid rgba(0, 70, 5, 1)",
-                color: "rgba(0, 70, 5, 1)",
-                borderRadius: "8px",
-                marginLeft: "5%",
-                marginTop: "5%",
-              }}
-            >
-              админы
-            </span>{" "}
-            <span
-              style={{
-                border: "1px solid rgba(0, 70, 5, 1)",
-                color: "rgba(0, 70, 5, 1)",
-                borderRadius: "8px",
-                marginLeft: "5%",
-                marginTop: "5%",
-              }}
-            >
-              трекеры
-            </span>{" "}
-            <span
-              style={{
-                border: "1px solid rgba(0, 70, 5, 1)",
-                color: "rgba(0, 70, 5, 1)",
-                borderRadius: "8px",
-                marginLeft: "5%",
-                marginTop: "5%",
-              }}
-            >
-              менторы
-            </span>
-            <span
-              style={{
-                border: "1px solid rgba(0, 70, 5, 1)",
-                color: "rgba(0, 70, 5, 1)",
-
-                borderRadius: "8px",
-                marginLeft: "5%",
-                marginTop: "5%",
-              }}
-              s
-            >
-              кураторы
-            </span>
-          </Grid>
-        </Grid>
-      </div>
-      <div style={{ display: "block", width: "100%", marginRight: "4px" }}>
-        <h1>Пользователи</h1>
-        <Grid container style={{ display: "flex", flexWrap: "wrap" }}>
-          {usersList.map((user) => (
             <Grid
               item
               style={{
-                backgroundColor: "rgba(217, 217, 217, 1)",
-                width: "300px",
-                alignContent: "center",
-                borderRadius: "8px",
+                width: "80%",
+                marginLeft: "8%",
+                fontSize: "14px",
+                fontWeight: "400",
+              }}
+            >
+              Фильтрация пользователей
+            </Grid>
+            <Grid
+              style={{
                 display: "flex",
                 flexWrap: "wrap",
-                paddingTop: "3%",
-                paddingBottom: "3%",
-                marginTop: "2%",
-                marginRight: "4%",
+                justifyContent: "space-evenly",
+                marginRight: "8%  ",
               }}
-              //   onClick={() => navigate("/userProfile")}
             >
-              <Grid
-                item
-                style={{ display: "flex", justifyContent: "space-around" }}
+              <span
+                style={{
+                  border: "1px solid rgba(0, 70, 5, 1)",
+                  color: "rgba(0, 70, 5, 1)",
+                  borderRadius: "8px",
+                  marginLeft: "5%",
+                  marginTop: "5%",
+                }}
+                onClick={() => {
+                  newUsers();
+                }}
               >
-                <Grid item>
-                  {/* <img src="#" alt="Эрнас “Сладкая душа”" /> */}
-                  <Avatar src={user.user_photo}></Avatar>
-                </Grid>
-                <Grid item style={{ marginLeft: "5%  ", width: "60%" }}>
-                  <div
-                    style={{
-                      color: "rgba(71, 71, 71, 1)",
-                      fontFamily: "Raleway",
-                      fontSize: "14px",
-                      fontWeight: "700",
-                      lineHeight: "14px",
-                      textAlign: "left",
-                    }}
-                  >
-                    <Link to={`/users/${user.id}`}>{user.username}</Link>
-                  </div>
-                  <div
-                    style={{
-                      fontFamily: "Raleway",
-                      fontSize: "12px",
-                      fontWeight: "400",
-                      lineLeight: "12px",
-                      letterSpacing: "0em",
-                      textAlign: "left",
-                      marginBottom: "3%",
-                    }}
-                  >
-                    {user.about_me}
-                  </div>
-                </Grid>
-              </Grid>
-              <div style={{ display: "block", width: "100%" }}>
-                <div
+                <div style={{ fontSize: "15px", padding: "2px" }}>новые</div>
+              </span>{" "}
+              <span
+                style={{
+                  border: "1px solid rgba(0, 70, 5, 1)",
+                  color: "rgba(0, 70, 5, 1)",
+                  borderRadius: "8px",
+                  marginLeft: "5%",
+                  marginTop: "5%",
+                }}
+                onClick={() => {
+                  isMentors();
+                }}
+              >
+                <div style={{ fontSize: "15px", padding: "2px" }}>менторы</div>
+              </span>
+              <span
+                style={{
+                  border: "1px solid rgba(0, 70, 5, 1)",
+                  color: "rgba(0, 70, 5, 1)",
+                  borderRadius: "8px",
+                  marginLeft: "5%",
+                  marginTop: "5%",
+                }}
+                onClick={() => {
+                  getUsers();
+                }}
+              >
+                <div style={{ fontSize: "15px", padding: "2px" }}>все</div>
+              </span>
+            </Grid>
+          </Grid>
+        </div>
+        <div style={{ display: "block", width: "80%", marginLeft: "5%" }}>
+          <h1>Пользователи</h1>
+          <Grid container style={{ display: "flex", flexWrap: "wrap" }}>
+            {state.users?.map((user) => {
+              return (
+                <Grid
+                  item
                   style={{
+                    backgroundColor: "rgba(217, 217, 217, 1)",
+                    width: "300px",
+                    alignContent: "center",
+                    borderRadius: "8px",
                     display: "flex",
                     flexWrap: "wrap",
-                    width: "100%",
-                    justifyContent: "space-around",
-                    fontSize: "10px",
+                    paddingTop: "3%",
+                    paddingBottom: "3%",
+                    paddingLeft: "1%",
+                    paddingRight: "1%",
+                    marginTop: "2%",
+                    marginRight: "4%",
                   }}
                 >
-                  <div style={{ color: " rgba(170, 104, 0, 1)" }}>
-                    Голосов:200
-                  </div>
-                  <div style={{ display: "flex", alignItems: "center" }}>
-                    <img src={Fire} alt="" />
-                    <div style={{ color: " rgba(170, 104, 0, 1)" }}>
-                      {user.is_fireman}3
+                  <Grid
+                    item
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-around",
+                    }}
+                  >
+                    <Grid item>
+                      {/* <img src="#" alt="Эрнас “Сладкая душа”" /> */}
+                      <Avatar
+                        src={user.user_photo}
+                        style={{
+                          marginLeft: "20%",
+                          marginBottom: "14%",
+                          width: "80px",
+                          height: "80px",
+                        }}
+                      ></Avatar>
+                    </Grid>
+                    <Grid item style={{ marginLeft: "20%  ", width: "80%" }}>
+                      <div
+                        style={{
+                          color: "rgba(71, 71, 71, 1)",
+                          fontFamily: "Raleway",
+                          fontSize: "14px",
+                          fontWeight: "700",
+                          lineHeight: "14px",
+                          textAlign: "left",
+                        }}
+                      >
+                        <Link to={`/users/${user.id}`}>{user.username}</Link>
+                      </div>
+                      <div
+                        style={{
+                          fontFamily: "Raleway",
+                          fontSize: "12px",
+                          fontWeight: "400",
+                          lineLeight: "12px",
+                          letterSpacing: "0em",
+                          textAlign: "left",
+                          marginBottom: "3%",
+                        }}
+                      >
+                        {user.about_me}
+                      </div>
+                    </Grid>
+                  </Grid>
+                  <div style={{ display: "block", width: "100%" }}>
+                    <div
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        width: "75%",
+                        justifyContent: "space-evenly",
+                        fontSize: "10px",
+                      }}
+                    >
+                      <div style={{ color: " rgba(170, 104, 0, 1)" }}>
+                        Голосов:200
+                      </div>
+                      <div style={{ display: "flex", alignItems: "center" }}>
+                        <img src={Fire} alt="" />
+                        <div style={{ color: " rgba(170, 104, 0, 1)" }}>
+                          {user.is_fireman}3
+                        </div>
+                      </div>
+                      <div style={{ display: "flex", alignItems: "center" }}>
+                        <img src={bita} alt="" />
+                        <div style={{ color: " rgba(170, 104, 0, 1)" }}>
+                          {user.is_mentor}5
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      style={{
+                        width: "94%",
+                        display: "flex",
+                        justifyContent: "space-evenly",
+                        marginTop: "4% ",
+                        alignContent: "center",
+                        alignItems: "center",
+                        textAlign: "center",
+                      }}
+                    >
+                      <div
+                        style={{
+                          backgroundColor: " rgba(170, 104, 0, 1)",
+                          color: "white",
+                          borderRadius: "5px",
+                          fontFamily: "Raleway",
+                          fontSize: "10px",
+                          fontWeight: "400",
+                          lineHeight: "15px",
+                          letterSpacing: "0em",
+                          textAlign: "center",
+                          width: "20%",
+                        }}
+                      >
+                        javascript
+                      </div>
+                      <div
+                        style={{
+                          backgroundColor: " rgba(170, 104, 0, 1)",
+                          color: "white",
+                          borderRadius: "5px",
+                          fontFamily: "Raleway",
+                          fontSize: "10px",
+                          fontWeight: "400",
+                          lineHeight: "15px",
+                          letterSpacing: "0em",
+                          textAlign: "center",
+                          width: "20%",
+                        }}
+                      >
+                        react
+                      </div>
+                      <div
+                        style={{
+                          backgroundColor: " rgba(170, 104, 0, 1)",
+                          color: "white",
+                          borderRadius: "5px",
+                          fontFamily: "Raleway",
+                          fontSize: "10px",
+                          fontWeight: "400",
+                          lineHeight: "15px",
+                          letterSpacing: "0em",
+                          textAlign: "center",
+                          width: "20%",
+                        }}
+                      >
+                        html
+                      </div>
                     </div>
                   </div>
-                  <div style={{ display: "flex", alignItems: "center" }}>
-                    <img src={bita} alt="" />
-                    <div style={{ color: " rgba(170, 104, 0, 1)" }}>
-                      {user.is_mentor}5
-                    </div>
-                  </div>
-                </div>
-                <div
-                  style={{
-                    width: "100%",
-                    display: "flex",
-                    justifyContent: "space-around",
-                    marginTop: "3% ",
-                  }}
-                >
-                  <div
-                    style={{
-                      backgroundColor: " rgba(170, 104, 0, 1)",
-                      color: "white",
-                      borderRadius: "5px",
-                      fontFamily: "Raleway",
-                      fontSize: "10px",
-                      fontWeight: "400",
-                      lineHeight: "15px",
-                      letterSpacing: "0em",
-                      textAlign: "center",
-                      width: "100%",
-                    }}
-                  >
-                    javascript
-                  </div>
-                  <div
-                    style={{
-                      backgroundColor: " rgba(170, 104, 0, 1)",
-                      color: "white",
-                      borderRadius: "5px",
-                      fontFamily: "Raleway",
-                      fontSize: "10px",
-                      fontWeight: "400",
-                      lineHeight: "15px",
-                      letterSpacing: "0em",
-                      textAlign: "center",
-                      width: "100%",
-                    }}
-                  >
-                    react
-                  </div>
-                  <div
-                    style={{
-                      backgroundColor: " rgba(170, 104, 0, 1)",
-                      color: "white",
-                      borderRadius: "5px",
-                      fontFamily: "Raleway",
-                      fontSize: "10px",
-                      fontWeight: "400",
-                      lineHeight: "15px",
-                      letterSpacing: "0em",
-                      textAlign: "center",
-                      width: "100%",
-                    }}
-                  >
-                    html
-                  </div>
-                </div>
-              </div>
-            </Grid>
-          ))}
-        </Grid>
+                </Grid>
+              );
+            })}
+          </Grid>
+        </div>
       </div>
     </Container>
   );
